1793ac514f1861e77c39ab083305f35041ec18ce
Michael (Doc) Norton <docondev@gmail.com>
2013-01-21 21:25:01 -0600

    Set X-Axis to datetime

--- public/javascripts/cfd.js
+++ public/javascripts/cfd.js
@@ -10,7 +10,9 @@ $(function () {
     chart = new Highcharts.Chart({
       chart: { renderTo: 'container', type: 'area', zoomType: 'xy' },
       title: { text: _chartTitle },
-      xAxis: { categories: _xCat, tickmarkPlacement: 'on', title: { enabled: false } },
+          // type: 'datetime',
+          //                 maxZoom: 14 * 24 * 3600000, // fourteen days
+      xAxis: { categories: _xCat, tickmarkPlacement: 'on', title: { enabled: false }, type: 'datetime' },
       yAxis: { title: { text: 'Points' } },
       tooltip: { formatter: function() { return ''+ this.x +': '+ Highcharts.numberFormat(this.y, 0, ',') +' points'; } },
       plotOptions: {
