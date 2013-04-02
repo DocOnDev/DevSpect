a258284548c440989690e7b1fc716ca6df74d8bb
Michael (Doc) Norton <docondev@gmail.com>
2013-04-02 13:56:21 -0400

    Improves zoom feature

--- public/javascripts/cfd.js
+++ public/javascripts/cfd.js
@@ -7,9 +7,16 @@ $(function () {
     _chartTitle = (typeof chartTitle != 'undefined' ? chartTitle : 'Cumulative Flow Diagram');
 
     chart = new Highcharts.Chart({
-      chart: { renderTo: 'container', type: 'area', zoomType: 'xy' },
+      chart: { renderTo: 'container', type: 'area', zoomType: 'x',
+        events: { 
+          selection: function(event) { 
+            event.target.yAxis[0].setExtremes(event.target.series[6].dataMin, event.target.yAxis.dataMax);
+          }
+        }
+      },
       title: { text: _chartTitle },
-      xAxis: { type: 'datetime', tickInterval: (24 * 3600 * 1000 ), 
+      xAxis: { type: 'datetime', tickInterval: (24 * 3600 * 1000 ), maxZoom: (7 * 24 * 3600000),
+        title: { text: null },
         dateTimeLabelFormats: {
            day: '%d-%b-%Y',
            week: '%d-%b-%Y',
@@ -18,20 +25,22 @@ $(function () {
        } },
       yAxis: { title: { text: 'Points' } },
       legend: {
-        layout: 'vertical',
+        layout: 'horizontal',
         backgroundColor: '#327aaf',
-        align: 'left',
-        verticalAlign: 'top',
-        x: 100,
-        y: 70,
-        floating: true,
         shadow: true
       },
-      tooltip: { formatter: function() { return ''+ Highcharts.dateFormat('%d-%b', this.x) +': '+ Highcharts.numberFormat(this.y, 0, ',') +' points'; } },
+      tooltip: { shared: true, valueSuffix: ' points' },
       plotOptions: {
         area: {
           stacking: 'normal',
           lineWidth: 1,
+        },
+        series: {
+          events: {
+            legendItemClick: function(event) {
+              if (this.name == 'done') return false;
+            }
+          }
         }
       },
       series: _dataSeries
